84b5bbd22cf8a63097233444387594eaba8c6e14
Torbie <jack@phthalo.net>
2024-11-18 13:48:31 -0500

    Changed Question System and Answer system to be more user friendly

--- quiz.py
+++ quiz.py
@@ -1,13 +1,33 @@
 # This is the main file for my Personal Quiz
 
-questionList = [
-    ("When is your birthday?(XX/XX/XXXX)", "08/20/2002"),
-    ("Who is the cutest cat ever?", "Mozelle")
-]
+questionsPlural = {
+    "When is your birthday?(XX/XX/XXXX)": [
+        "08/20/2002", "02/18/2002", "10/12/2005", "04/16/1975"
+        ],
+    "Who is the cutest cat ever?": [
+        "Mozelle", "Skippy", "Jimbo", "Bro"
+    ],
+    "What does Will get at Sushi mon?": [
+        "Udon", "Chicken Teryiaki", "Miso Soup", "A Big Block of Cheese"
+    ],
+    "What is the best fallout game?": [
+        "Fallout 4", "Fallout 3", "Fallout New Vegas", "Fallout 76"
+    ],
 
-for question, correct_answer in questionList:
-    answer = input(f"{question}? ")
+}
+
+
+for question, alternatives in questionsPlural.items():
+    correct_answer = alternatives[0]
+    sorted_alternatives = sorted(alternatives)
+    for label, alternative in enumerate(sorted_alternatives):
+        print(f"    {label}) {alternative}")
+
+    answer_label = int(input(f"{question} "))
+    answer = sorted_alternatives[answer_label]
     if answer == correct_answer:
-        print("Oh wow, you're so right!")
+        print("Correct!")
     else:
-        print(f"Dumbass, study up! The correct answer is {correct_answer!r}! Not {answer!r}!")
+        print(f"The answer is {correct_answer!r}, not {answer!r}!")
+
+
